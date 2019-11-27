13b3f61dcac444781026bbf100c0a3f114a4e36d
Cesar Acual <chechaacual@gmail.com>
2019-11-27 13:06:41 -0600

    Configuration Spring Boot

--- src/main/java/app/messages/Application.java
+++ src/main/java/app/messages/Application.java
@@ -1,15 +1,14 @@
 package app.messages;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Application.java is the entry point of the application.
  */
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-        MessageService messageService = context.getBean(MessageService.class);
-        messageService.save("Hello, Spring!!!");
+        SpringApplication.run(Application.class);
     }
 }
