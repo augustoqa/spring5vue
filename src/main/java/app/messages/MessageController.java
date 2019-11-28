496e78521d1ada4fd079d7fa056acb3afd784f39
Cesar Acual <chechaacual@gmail.com>
2019-11-28 11:03:40 -0600

    Another way to utilize the model and view

--- src/main/java/app/messages/MessageController.java
+++ src/main/java/app/messages/MessageController.java
@@ -1,17 +1,18 @@
 package app.messages;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/messages")
 public class MessageController {
 
     @GetMapping("/welcome")
-    public String welcome(Model model) {
-        model.addAttribute("message", "Hello, Welcome to Spring Boot!");
-        return "welcome";
+    public ModelAndView welcome() {
+        ModelAndView mv = new ModelAndView("welcome");
+        mv.addObject("message", "Hello, Welcome to Spring Boot!");
+        return mv;
     }
 }
