0b5de1a1238106946988c8d9dfdaa7a56c4218e2
Cesar Acual <chechaacual@gmail.com>
2019-11-28 12:15:24 -0600

    A example filter

--- src/main/java/app/messages/AppConfig.java
+++ src/main/java/app/messages/AppConfig.java
@@ -1,8 +1,11 @@
 package app.messages;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
 
 /**
  * AppConfig.java is the configuration metadata Spring will use to instantiate the container
@@ -13,5 +16,15 @@ import org.springframework.context.annotation.Configuration;
 //@Configuration to tell Spring the base package for scanning for annotated components.
 @ComponentScan("app.messages")
 public class AppConfig {
+
+    @Bean
+    public FilterRegistrationBean<AuditingFilter> auditingFilterRegistrationBean() {
+        FilterRegistrationBean<AuditingFilter> registration = new FilterRegistrationBean<>();
+        AuditingFilter filter = new AuditingFilter();
+        registration.setFilter(filter);
+        registration.setOrder(Integer.MAX_VALUE);
+        registration.setUrlPatterns(Arrays.asList("/messages/*"));
+        return registration;
+    }
 
 }
